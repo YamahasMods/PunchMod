5ddb0300338a0d8ba3e7f08b0b442fd2f7cbe6a0
Yamaha <ari.arikid@gmail.com>
2024-05-08 16:10:52 -0700

    Create PluginInfo.cs

---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZenonsModTemplate
+{
+    public class PluginInfo
+    {
+        public const string GUID = "YAMAHA.modded_punchmod";
+        public const string Name = "YAMAHAs MODDED PUNCH MOD";
+        public const string Version = "2.0.0";
+    }
+}
